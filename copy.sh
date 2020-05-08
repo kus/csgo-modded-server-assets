5b3d796c3df3d1fe374439ed38a0414e028b791b
Blake Kus <blakekus@gmail.com>
2020-05-08 16:09:27 +1000

    - ADDED: .nav files - UPDATED: Copy script to copy non bz2 files to server

--- copy.sh
+++ copy.sh
@@ -11,6 +11,7 @@ then
 	exit 1
 fi
 
+# Decompress bz2 files
 array=($(find csgo/maps csgo/sound csgo/materials csgo/models -type f \( -name "*.bz2" \)))
 for i in ${array[@]};
 do
@@ -22,4 +23,18 @@ do
 		mkdir -p ../$DIR
 	fi
 	bzip2 -cd $i > ../$FILE;
+done
+
+# Copy other files
+array=($(find csgo/maps csgo/sound csgo/materials csgo/models -type f \( ! -name ".DS_Store" -and ! -name "*.bz2" \)))
+for i in ${array[@]};
+do
+	DIR=$(dirname "${i}")
+	FILE=${i}
+	echo "Copying $i"
+	if [ ! -d ../$DIR ]
+	then
+		mkdir -p ../$DIR
+	fi
+	cp -n $i ../$FILE
 done
